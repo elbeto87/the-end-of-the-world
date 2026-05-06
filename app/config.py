a53fd56d21fb1c16b05605b4c4424451c76d1a19
Ezequiel Gresia <gresia.ezequiel@gmail.com>
2026-05-06 15:49:19 -0300

    fixing settings variables

--- app/config.py
+++ app/config.py
@@ -3,8 +3,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     NASA_API_KEY: str = "DEMO_KEY"
-    NASA_NEOWS_BASE_URL: str = "https://api.nasa.gov"
-    NASA_SENTRY_JPL_BASE_URL: str = "https://api.nasa.gov"
+    NASA_NEOWS_BASE_URL: str = "https://api.nasa.gov/neo/rest/v1"
+    NASA_SENTRY_JPL_BASE_URL: str = "https://ssd-api.jpl.nasa.gov/sentry.api"
 
     class Config:
         env_file = ".env"
